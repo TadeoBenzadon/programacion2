f79539dd906d93e6b0f0a0b87904a4c05bed6f62
TadeoBenzadon <tbenzadon@udesa.edu.ar>
2022-04-11 10:21:41 -0300

    Termine el index

--- proyectoFinal/controllers/indexController.js
+++ proyectoFinal/controllers/indexController.js
@@ -1,10 +1,10 @@
+const products = require("../db/products");
 const data = require("../db/products")
 
 const indexController = { 
     index: function(req,res){
-        return res.render('index', {
-
-       })
+        return res.render('index', {productos: products.lista})
+           
     }
 }
 
--- proyectoFinal/db/products.js
+++ proyectoFinal/db/products.js
@@ -1,62 +1,62 @@
 const products = {
     lista:[
         {
-        //imagen:,
+        imagen: "/stylesheets/images/heladera.png",
         nombre: "Heladera",
         descripcion:"Heladera Electrolux Inverter 4 puertas modelo DM90X",
         fechaDeCarga: 3/04/2022
     },
     {
-        //imagen:,
+        imagen: "/stylesheets/images/horno.png",
         nombre: "Horno",
         descripcion:"Cocina Electrolux doble horno, grill, panel digital de 5 hornallas modelo 76DXR",
         fechaDeCarga: 3/04/2022,
     },
     {
-        //imagen:,
+        imagen: "/stylesheets/images/pavaElectrica.png",
         nombre: "Pava electrica",
         descripcion:"Pava Electrica Smartlife Sl-ek1714b 1,7l",
         fechaDeCarga: 3/04/2022,
     },
     {
-        //imagen:,
-        nombre: "Cafetera HOLAAAAA",
+        imagen: "/stylesheets/images/cafetera.png",
+        nombre: "Cafetera",
         descripcion:"cafetera liliana ac950 cofly", 
         fechaDeCarga: 3/04/2022,
     },
     {
-        //imagen:,
+        imagen: "/stylesheets/images/microondas.png",
         nombre: "Microondas",
         descripcion:"Microondas Midea de 20 litros color gris modelo MMD20AS5AM",
         fechaDeCarga: 3/04/2022,
     },
     {
-        //imagen:,
-        nombre: "aspiradora",
+        imagen: "/stylesheets/images/aspiradora.png",
+        nombre: "Aspiradora",
         descripcion:"Aspiradora industrial profesional karcher nt 27/1 antracita", 
         fechaDeCarga: 3/04/2022,
     },
     {
-        //imagen:,
-        nombre: "batidora",
+        imagen: "/stylesheets/images/batidora.png",
+        nombre: "Batidora",
         descripcion:"Batidora planetaria art . sm3711 oster",
         fechaDeCarga: 3/04/2022,
     },
     {
-        //imagen:,
-        nombre: "plancha",
+        imagen: "/stylesheets/images/plancha.png",
+        nombre: "Plancha",
         descripcion:"Plancha a Vapor Philips EasySpeed GC-1022-40 2000W",
         fechaDeCarga: 3/04/2022,
     },
     {
-        //imagen:,
-        nombre: "balanza",
+        imagen: "/stylesheets/images/balanza.png",
+        nombre: "Balanza",
         descripcion:"Balanza Digital De Cocina Electronica 10kg",
         fechaDeCarga: 3/04/2022,
     },
     {
-        //imagen:,
-        nombre: "tostadora",
+        imagen: "/stylesheets/images/tostadora.png",
+        nombre: "Tostadora",
         descripcion:"Tostadora Eléctrica Atma To8020i 700w 7 Niveles Full",
         fechaDeCarga: 3/04/2022,
     }
