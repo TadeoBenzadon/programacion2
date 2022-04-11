339a0fdcfc5b60f89840fe62d6dd187074283705
j29f123 <jfreiman@udesa.edu.ar>
2022-04-11 09:06:13 -0300

    fotos y descripcion productos

--- proyectoFinal/db/products.js
+++ proyectoFinal/db/products.js
@@ -3,61 +3,61 @@ const products = {
         {
         //imagen:,
         nombre: "Heladera",
-        descripcion:"",
+        descripcion:"Heladera Electrolux Inverter 4 puertas modelo DM90X",
         fechaDeCarga: 3/04/2022
     },
     {
         //imagen:,
         nombre: "Horno",
-        descripcion:"",
+        descripcion:"Cocina Electrolux doble horno, grill, panel digital de 5 hornallas modelo 76DXR",
         fechaDeCarga: 3/04/2022,
     },
     {
         //imagen:,
         nombre: "Pava electrica",
-        descripcion:"",
+        descripcion:"Pava Electrica Smartlife Sl-ek1714b 1,7l",
         fechaDeCarga: 3/04/2022,
     },
     {
         //imagen:,
         nombre: "Cafetera",
-        descripcion:"",
+        descripcion:"cafetera liliana ac950 cofly", 
         fechaDeCarga: 3/04/2022,
     },
     {
         //imagen:,
         nombre: "Microondas",
-        descripcion:"",
+        descripcion:"Microondas Midea de 20 litros color gris modelo MMD20AS5AM",
         fechaDeCarga: 3/04/2022,
     },
     {
         //imagen:,
         nombre: "aspiradora",
-        descripcion:"",
+        descripcion:"Aspiradora industrial profesional karcher nt 27/1 antracita", 
         fechaDeCarga: 3/04/2022,
     },
     {
         //imagen:,
         nombre: "batidora",
-        descripcion:"",
+        descripcion:"Batidora planetaria art . sm3711 oster",
         fechaDeCarga: 3/04/2022,
     },
     {
         //imagen:,
         nombre: "plancha",
-        descripcion:"",
+        descripcion:"Plancha a Vapor Philips EasySpeed GC-1022-40 2000W",
         fechaDeCarga: 3/04/2022,
     },
     {
         //imagen:,
         nombre: "balanza",
-        descripcion:"",
+        descripcion:"Balanza Digital De Cocina Electronica 10kg",
         fechaDeCarga: 3/04/2022,
     },
     {
         //imagen:,
         nombre: "tostadora",
-        descripcion:"",
+        descripcion:"Tostadora Eléctrica Atma To8020i 700w 7 Niveles Full",
         fechaDeCarga: 3/04/2022,
     }
 ]
