e4474c8dab8e39fca3c71fec021446616e667397
j29f123 <jfreiman@udesa.edu.ar>
2022-06-06 09:04:47 -0300

    products

---
+++ proyectoFinal/database/models/Product.js
@@ -0,0 +1,34 @@
+module.exports(sequalize, dataTypes)
+let alias="products";
+let cols= {
+    id_product:{
+        type:dataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false},
+    
+    imagen_product:{
+        type: dataTypes.STRING,
+        allowNull: false},
+    
+    name_product:{
+        type: dataTypes.STRING,
+        allowNull: false},
+        category:{
+            type:dataTypes:STRING
+        },
+    
+    product_descripcion:{
+        type: dataTypes.STRING,
+        allowNull: false},
+    
+    product_fechaDeCarga:{
+        type: dataTypes.STRING,
+        allowNull: false},  
+    
+}
+
+let config={
+    table_name: "products",
+    timesTamps: false
+}
