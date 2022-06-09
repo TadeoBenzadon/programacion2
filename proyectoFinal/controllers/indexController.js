1d9459707febfc900962ff14ebf41341bdd29846
angieamorena <mamorena@udesa.edu.ar>
2022-06-09 16:11:46 -0300

    index controller

--- proyectoFinal/controllers/indexController.js
+++ proyectoFinal/controllers/indexController.js
@@ -12,28 +12,16 @@ const indexController = {
                 include:{association: 'user'}}
            ]})
 
-           .then(function(nuevos) 
-           {db.Product.findAll({
-            order: [['electro_comments', 'DESC']],
-            limit: 12, 
-            include: [
-                {association: 'user'},
-                {association: 'comment',
-                 include:{association: 'user'}}]
-           }) 
-
-           .then(function(comentados)
+           .then(function(productos)
            {return res.render('index',{
-               productos: nuevos,
-               comentados: comentados,
+               productos: productos,
                title: 'Janise Market'
            })})
            
            .catch(function(error){
                console.log(error);
            })
-        })
+        }
     }
-}
 
 module.exports = indexController; 
